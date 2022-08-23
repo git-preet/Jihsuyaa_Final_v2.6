aa49dc9ac75f06f26bf930b91f6810c8f7df9f3f
Akshar Virani <83912915+aksharvirani@users.noreply.github.com>
2022-08-23 18:35:08 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
     if(isset($_POST['submit']))
     {
        $name = $_POST['name'];
-       $email = $_POST['mail'];
+       $email = $_POST['email'];
        $subject = $_POST['subject'];
        $message = $_POST['message'];
 
